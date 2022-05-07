94a2b6feb8eee14b2627041ce87c4a53c301fd75
dhaneshvg <dhanesh.v.gddd@gmail.com>
2022-05-07 22:19:37 +0530

    First commit

---
+++ ExceptionLogToTextFile/Index.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ExceptionLogToTextFile.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ExceptionLogToTextFile/Index.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace ExceptionLogToTextFile
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/abcd.xml"));
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            catch(Exception ex)
+            {
+                ExceptionLogging.SendErrorToText(ex);
+                Label1.Text="Some Technical Error occured, Please Visit after sometime";
+            }
+        }
+    }
+}
